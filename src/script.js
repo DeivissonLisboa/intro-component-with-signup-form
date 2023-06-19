4ea7d5229bb4d18d78017b0ec4584b86e4c10d39
Deivisson Lisboa <dvilaflor@gmail.com>
2023-06-19 13:48:39 -0300

    Add and style form validation

--- src/script.js
+++ src/script.js
@@ -1,11 +1,22 @@
 const FORM = document.querySelector("form")
 const INPUTS = document.querySelectorAll(".form--input")
 
+function validateEmail(email) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+  return emailRegex.test(email)
+}
+
 FORM.addEventListener("submit", (e) => {
   e.preventDefault()
+
   INPUTS.forEach((input) => {
-    if (!input.value) {
-      console.log("Hello world")
+    input.classList.remove("error")
+  })
+
+  INPUTS.forEach((input) => {
+    console.log(input.id)
+    if (!input.value || (input.id === "email" && !validateEmail(input.value))) {
+      input.classList.add("error")
     }
   })
 })
